93840cee089512116e671d726738421978cb573e
Никита <klosr@list.ru>
2020-06-18 03:36:10 +0300

    Добавил пропсы

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Main from '../main/main.jsx';
 
-const App = () => {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {offersAmount} = props;
+
   return (
-    <Main />
+    <Main offersAmount={offersAmount} />
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
+const Settings = {
+  OFFERS_AMOUNT: 312
+};
+
 ReactDOM.render(
-    <App />,
+    <App offersAmount={Settings.OFFERS_AMOUNT} />,
     document.querySelector(`#root`)
 );
