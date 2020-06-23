e324d1d825b17712d7109fcef92073a4c15614ae
Никита <klosr@list.ru>
2020-06-23 22:45:54 +0300

    Добавил снэпшот тесты

---
+++ src/components/main/main.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Main from './main.jsx';
+
+const Settings = {
+  OFFERS_AMOUNT: 312
+};
+
+const offers = [
+  {title: `Beautiful & luxurious apartment at great location`},
+  {title: `Wood and stone place`},
+  {title: `Canal View Prinsengracht`},
+  {title: `Nice, cozy, warm big bed apartment`},
+  {title: `Wood and stone place`}
+];
+
+it(`Should Main render correctly`, () => {
+  const tree = renderer
+    .create(<Main
+      offersAmount={Settings.OFFERS_AMOUNT} offers={offers}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
