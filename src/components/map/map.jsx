32453364c07047a3ded2f58462889c7a9e0ec896
Никита <klosr@list.ru>
2020-07-21 17:29:49 +0300

    Добавил PropTypes в Map, подредактировал компонент

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,92 +1,52 @@
 import React from 'react';
-import leaflet from 'leaflet';
+import L from 'leaflet';
+import PropTypes from 'prop-types';
 
+const city = [52.38333, 4.9];
+const zoom = 12;
 
-// const Map = (props) => {
-
-//   const mapRef = React.createRef();
-
-// const city = [52.38333, 4.9];
-
-//   const icon = leaflet.icon({
-//     iconUrl: `img/pin.svg`,
-//     iconSize: [30, 30]
-//   });
-
-//   const zoom = 12;
-
-//   const map = leaflet.map(`map`, {
-//     center: city,
-//     zoom,
-//     zoomControl: false,
-//     marker: true
-//   });
-//   map.setView(city, zoom);
-
-//   leaflet
-//     .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-//       attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-//     })
-//     .addTo(map);
-
-//   const offerCords = [52.3709553943508, 4.89309666406198];
-
-//   leaflet
-//   .marker(offerCords, {icon})
-//   .addTo(map);
-// };
-
-class Map extends React.PureComponent {
+export default class Map extends React.PureComponent {
   constructor(props) {
     super(props);
     this.mapRef = React.createRef();
   }
 
   componentDidMount() {
-    const icon = leaflet.icon({
+    const icon = L.icon({
       iconUrl: `img/pin.svg`,
       iconSize: [30, 30]
     });
 
-    // const offersCords = this.props.offers.map((offer) => [offer.location.latitude, offer.location.longitude]);
-    // console.log(offersCords);
-
-    this.map = leaflet.map(this.mapRef.current, {
-      center: [52.38333, 4.9],
-      zoom: 12,
+    this.map = L.map(this.mapRef.current, {
+      center: city,
+      zoom,
       zoomControl: false,
       marker: true
     });
+    this.map.setView(city, zoom);
 
-    leaflet
-      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-      })
+    L.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+    })
       .addTo(this.map);
 
-    this.props.offers.map((offer) => {
-      leaflet.marker([offer.location.latitude, offer.location.longitude], {icon}).addTo(this.map);
+    this.props.offers.forEach(({location}) => {
+      L.marker([location.latitude, location.longitude], {icon}).addTo(this.map);
     });
-    // this.offersLayer = leaflet.layerGroup(offersCords, {}).addTo(this.map);
-
-    // const offerCords = [52.3709553943508, 4.89309666406198];
-
-    // leaflet
-    //   .marker(offerCords, {icon})
-    //   .addTo(this.map);
   }
 
   render() {
     return (
-      // <section className="cities__map map">
       <div id="map" ref={this.mapRef} className="cities__map"></div>
-      // </section>
-
-    // <section className="cities__map map">
-    //   <div id="map" ref={this.mapRef} className="cities__map"></div>
-    // </section>
     );
   }
 }
 
-export default Map;
+Map.propTypes = {
+  offers: PropTypes.arrayOf(PropTypes.shape({
+    location: PropTypes.shape({
+      latitude: PropTypes.number.isRequired,
+      longitude: PropTypes.number.isRequired
+    })
+  }))
+};
