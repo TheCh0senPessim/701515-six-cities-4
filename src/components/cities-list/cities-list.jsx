566d91fccc8f177544f713fd25cadc64ea0741bd
Никита <klosr@list.ru>
2020-07-24 17:35:09 +0300

    Добавил отображение No places

---
+++ src/components/cities-list/cities-list.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const citiesList = (props) => {
+  return (
+    <div>{props.city}</div>
+  );
+}
+
+export default citiesList;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -31,7 +31,7 @@ const Main = (props) => {
         </div>
       </header>
 
-      <main className="page__main page__main--index">
+      <main className={`page__main page__main--index ${offers ? `page__main--index-empty` : ``}`}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -70,34 +70,44 @@ const Main = (props) => {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{offersAmount} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
+          { offers ? (
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{offersAmount} places to stay in Amsterdam</b>
+                <form className="places__sorting" action="#" method="get">
+                  <span className="places__sorting-caption">Sort by</span>
+                  <span className="places__sorting-type" tabIndex="0">
                 Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
-              </form>
-              <PlacesList offers={offers} onCardTitleClick={onCardTitleClick}/>
-            </section>
-            <div className="cities__right-section">
-              {/* <section className="cities__map map">
+                    <svg className="places__sorting-arrow" width="7" height="4">
+                      <use xlinkHref="#icon-arrow-select"></use>
+                    </svg>
+                  </span>
+                  <ul className="places__options places__options--custom places__options--opened">
+                    <li className="places__option places__option--active" tabIndex="0">Popular</li>
+                    <li className="places__option" tabIndex="0">Price: low to high</li>
+                    <li className="places__option" tabIndex="0">Price: high to low</li>
+                    <li className="places__option" tabIndex="0">Top rated first</li>
+                  </ul>
+                </form>
+                <PlacesList offers={offers} onCardTitleClick={onCardTitleClick}/>
+              </section>
+              <div className="cities__right-section">
+                {/* <section className="cities__map map">
                 {/* <Map offers={offers} /> */}
-              {/* </section> */}
-              <Map offers={offers} />
-            </div>
-          </div>
+                {/* </section> */}
+                <Map offers={offers} />
+              </div>
+            </div>) : (
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">We could not find any property availbale at the moment in Dusseldorf</p>
+                </div>
+              </section>
+              <div className="cities__right-section"></div>
+            </div>)}
         </div>
       </main>
     </div>
